7f99303f01b1d2d4866aed8fdf0817daaf4a73d2
ryanfiorini <ryanwfiorini@gmail.com>
2013-01-11 10:08:19 -0500

    added documentation

--- SimpleCalculator/Subtract.cs
+++ SimpleCalculator/Subtract.cs
@@ -1,4 +1,10 @@
-﻿using PublicInterfaces;
+﻿/*
+ * This example was taken from Microsofts msdn site.
+ * http://msdn.microsoft.com/en-us/library/dd460648.aspx?cs-save-lang=1&cs-lang=csharp#code-snippet-9
+ * 
+ */
+
+using PublicInterfaces;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.Composition;
